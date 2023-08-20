b9d55dff76a1f86946ad13412db3cbea708e4dc9
JerryWski <jarek_wasilewski@yahoo.com>
2023-08-20 17:05:38 +0200

    styling form for desktop

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -3,7 +3,7 @@ import styles from './FormSection.module.css';
 
 const FormSection = () => {
   return (
-    <div>
+    <div className={styles.form_section}>
       <form id="myForm" className={styles.form_container}>
         <div className={styles.form_header_wrapper}>
           <img
@@ -17,95 +17,109 @@ const FormSection = () => {
         </div>
 
         <div className={styles.inputs_wrapper}>
-          <label htmlFor="email" id="email" />
-          <input
-            className={styles.form_inputs}
-            placeholder=" Wpisz swój e-mail"
-            type="email"
-            id="email"
-            name="email"
-            required
-          />
-          <label htmlFor="phone" id="phone" />
-          <input
-            className={styles.form_inputs}
-            placeholder=" + 48   numer telefonu"
-            type="tel"
-            id="phone"
-            name="phone"
-            required
-          />
-          <div className={styles.checbox_container}>
-            <input
-              className={styles.form_checkbox}
-              type="checkbox"
-              id="option1"
-              name="option"
-              value="Option 1"
-              required
+          <div className={styles.input_image__container}>
+            <img
+              className={styles.input_image}
+              src="./email.svg"
+              alt="envleope-icon"
             />
-            <label className={styles.checkbox_label}>
-              ZAZNACZ WSZYSTKIE ZGODY
-            </label>
           </div>
-          <div className={styles.checbox_container}>
-            <input
-              className={styles.form_checkbox}
-              type="checkbox"
-              id="option2"
-              name="option"
-              value="Option 2"
-              required
-            />
-            <label className={styles.checkbox_label}>
-              Zapoznałem się z{' '}
-              <a className={styles.policy_links} href="/">
-                regulaminem
-              </a>{' '}
-              i{' '}
-              <a className={styles.policy_links} href="/">
-                polityką prywatności.
-              </a>
-            </label>
-          </div>
-          <div className={styles.checbox_container}>
-            <input
-              className={styles.form_checkbox}
-              type="checkbox"
-              id="option3"
-              name="option"
-              value="Option 3"
-              required
-            />
-            <label className={styles.checkbox_label}>
-              Wyrażam zgodę na otrzymywanie od SPEŁNIJ MARZENIA treści
-              marketingowych dotyczących organizowanych loterii, z
-              wykorzystaniem automatycznych systemów wywołujących, drogą SMS.
-            </label>
+          <div className={styles.inputs_container}>
+            <div className={styles.data_wrapper}>
+              <label htmlFor="email" id="email" />
+              <input
+                className={styles.form_inputs}
+                placeholder=" Wpisz swój e-mail"
+                type="email"
+                id="email"
+                name="email"
+                required
+              />
+              <label htmlFor="phone" id="phone" />
+              <input
+                className={styles.form_inputs}
+                placeholder=" + 48   numer telefonu"
+                type="tel"
+                id="phone"
+                name="phone"
+                required
+              />
+            </div>
+            <div className={styles.checbox_container}>
+              <input
+                className={styles.form_checkbox}
+                type="checkbox"
+                id="option1"
+                name="option"
+                value="Option 1"
+                required
+              />
+              <label className={styles.checkbox_label}>
+                ZAZNACZ WSZYSTKIE ZGODY
+              </label>
+            </div>
+            <div className={styles.checbox_container}>
+              <input
+                className={styles.form_checkbox}
+                type="checkbox"
+                id="option2"
+                name="option"
+                value="Option 2"
+                required
+              />
+              <label className={styles.checkbox_label}>
+                Zapoznałem się z{' '}
+                <a className={styles.policy_links} href="/">
+                  regulaminem
+                </a>{' '}
+                i{' '}
+                <a className={styles.policy_links} href="/">
+                  polityką prywatności.
+                </a>
+              </label>
+            </div>
+            <div className={styles.checbox_container}>
+              <input
+                className={styles.form_checkbox}
+                type="checkbox"
+                id="option3"
+                name="option"
+                value="Option 3"
+                required
+              />
+              <label className={styles.checkbox_label}>
+                Wyrażam zgodę na otrzymywanie od SPEŁNIJ MARZENIA treści
+                marketingowych dotyczących organizowanych loterii, z
+                wykorzystaniem automatycznych systemów wywołujących, drogą SMS.
+              </label>
+            </div>
+            <button
+              className={styles.form_button}
+              type="submit"
+              id="submitButton"
+            >
+              Powiadom mnie!{' '}
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-chevrons-right"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="white"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M7 7l5 5l-5 5" />
+                  <path d="M13 7l5 5l-5 5" />
+                </svg>
+              </span>
+            </button>
           </div>
         </div>
-        <button className={styles.form_button} type="submit" id="submitButton">
-          Powiadom mnie!{' '}
-          <span>
-            {/* <img src="./arrows.svg" alt="arrow-icon" /> */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-chevrons-right"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="white"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M7 7l5 5l-5 5" />
-              <path d="M13 7l5 5l-5 5" />
-            </svg>
-          </span>
-        </button>
       </form>
     </div>
   );
