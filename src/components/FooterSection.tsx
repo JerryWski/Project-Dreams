690394030bba9ca6fc451b5291710a56284e6c46
JerryWski <jarek_wasilewski@yahoo.com>
2023-08-21 20:37:59 +0200

    adding text to footer

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -4,6 +4,11 @@ const FooterSection = () => {
   return (
     <div className={styles.footer_container}>
       <img className={styles.footer_image} src="./star_3.svg" alt="" />
+      <p className={styles.footer_text}>
+        {' '}
+        Spełnij Marzenia sp. z o.o. ul. Henryka Wolińskiego 17/ LU2 20-491
+        Lublin
+      </p>
     </div>
   );
 };
