a872c343976f08239a503b1a968e00ced1e24a21
JerryWski <jarek_wasilewski@yahoo.com>
2023-08-22 12:19:09 +0200

    house trans

--- src/components/HouseContainer.tsx
+++ src/components/HouseContainer.tsx
@@ -1,6 +1,8 @@
+import { useTranslation } from 'react-i18next';
 import styles from './HouseContainer.module.css';
 
 const HouseContainer = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.house_container}>
       <picture>
@@ -13,22 +15,30 @@ const HouseContainer = () => {
       </picture>
       <div className={styles.house_description}>
         <h2 className={styles.description_header}>
-          Wygraj nowe mieszkanie i … kasę na urządzenie o łącznej wartości
-          1.000.000 złotych
+          {t(
+            'Win a new flat and...cash for an appliance worth a total of PLN 1,000,000'
+          )}
         </h2>
         <span className={styles.redline} />
         <p className={styles.description_text}>
-          Weź udział i wygraj mieszkanie o wartości 800 tys. złotych oraz 200
-          tys. złotych na jego wyposażenie!{' '}
-          <span className={styles.description_span}>
+          {t(
+            'Have you always dreamt of your own flat but the galloping inflation, the property prices and the credit terms take it all away from you? If so – our promotional lottery is cut out for you!'
+          )}{' '}
+          {/* <span className={styles.description_span}>
             {' '}
             Sam zdecydujesz gdzie będziesz mieszkał i jak będziesz mieszkał!
             Koszty podatku od nagrody ponosi organizator loterii!
-          </span>
+          </span> */}
         </p>
         <p className={styles.description_text}>
-          Sam zdecydujesz gdzie będziesz mieszkał i jak będziesz mieszkał!
-          Koszty podatku od nagrody ponosi organizator loterii!
+          {t(
+            'Take part in the lottery and win a flat worth PLN 800, 000, and PLN 200, 000 for its equipment!'
+          )}
+        </p>
+        <p className={styles.description_text}>
+          {t(
+            'You decide where and how you want to live! The costs of the tax are borne by the lottery organiser!'
+          )}
         </p>
       </div>
     </div>
