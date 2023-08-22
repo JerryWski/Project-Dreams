14fa5e4f651a8f76c3b3c713d8bfeca0098a2990
JerryWski <jarek_wasilewski@yahoo.com>
2023-08-22 16:14:45 +0200

    working

--- src/utils/SwiperModule.tsx
+++ src/utils/SwiperModule.tsx
@@ -1,11 +1,21 @@
 /* eslint-disable react/jsx-boolean-value */
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useTranslation } from 'react-i18next';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import './swiper.css';
 // import 'swiper/css/pagination';
 // import 'swiper/css/navigation';
 
 const SwiperModule = () => {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+
+  const imagePath = `/${
+    currentLanguage === 'en' ? 'slider_1_eng.jpg' : 'slider_1.jpg'
+  }`;
+  const imagePathTwo = `/${
+    currentLanguage === 'en' ? 'slider_2_eng.jpg' : 'slider_2.jpg'
+  }`;
   return (
     <Swiper
       spaceBetween={30}
@@ -23,14 +33,14 @@ const SwiperModule = () => {
     >
       <SwiperSlide>
         <picture>
-          <source srcSet="./slider_1.jpg" media="(min-width: 800px)" />
-          <img src="./slider_1_mobile.jpg" alt="cars" />
+          <source srcSet={imagePath} media="(min-width: 800px)" />
+          <img src={imagePath} alt="cars" />
         </picture>
       </SwiperSlide>
       <SwiperSlide>
         <picture>
-          <source srcSet="./slider_2.jpg" media="(min-width: 800px)" />
-          <img src="./slider_2_mobile.jpg" alt="cars" />
+          <source srcSet={imagePathTwo} media="(min-width: 800px)" />
+          <img src={imagePathTwo} alt="cars" />
         </picture>
       </SwiperSlide>
     </Swiper>
